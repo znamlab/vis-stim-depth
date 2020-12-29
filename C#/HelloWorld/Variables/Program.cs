86b362e2cd506b6c9d1c8b1b32c5e2d0c0ee6981
yiran25 <yiran.he@crick.ac.uk>
2020-12-29 21:39:35 +0000

    How to calculate XYZ based on azimuth and latitute. This can also be done in Bonsai scripts.

---
+++ C#/HelloWorld/Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            byte number = 1;
+            Console.WriteLine(number);  //Console is a class of the namespace System
+        }
+    }
+}
