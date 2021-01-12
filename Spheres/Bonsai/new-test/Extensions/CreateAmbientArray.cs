abdc4a86f086564df2d8bc28a405d8e310a9d8c7
yiran25 <yiran.he@crick.ac.uk>
2021-01-12 22:45:33 +0000

    Add black/white spheres. Update MouseZ sampling method.

---
+++ Spheres/Bonsai/new-test/Extensions/CreateAmbientArray.cs
@@ -0,0 +1,60 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class CreateAmbientArray
+{
+    public IObservable<float[]> Process(IObservable<int> source)
+    {
+        return source.Select(value => 
+        {
+            float start_amb = 0.5f;
+            int target_amb;
+            int framerate = value;
+            float totalTime = 4f;
+            float fadeinTime = 0.5f;
+            float fadeoutTime = 0.5f;
+            float stayTime = totalTime - fadeinTime - fadeoutTime;
+
+            Random random = new Random();
+            target_amb = random.Next(0,2); //create randomly a white or black sphere
+            float target_ambf = Convert.ToSingle(target_amb);
+
+            int fadein_frameNum = Convert.ToInt32(framerate * fadeinTime);
+            int stay_frameNum = Convert.ToInt32(framerate * stayTime);
+            int fadeout_frameNum = Convert.ToInt32(framerate * fadeoutTime);
+            int total_frameNum = fadein_frameNum + stay_frameNum + fadeout_frameNum;
+
+            float[] fadein_arr = linspace(start_amb,target_ambf,fadein_frameNum);
+            float[] stay_arr = Enumerable.Repeat(target_ambf, stay_frameNum).ToArray();
+            float[] fadeout_arr = linspace(target_ambf,start_amb,fadein_frameNum);
+
+            float[] result = new float[total_frameNum];
+            fadein_arr.CopyTo(result,0);
+            stay_arr.CopyTo(result,fadein_frameNum);
+            fadeout_arr.CopyTo(result,fadein_frameNum+stay_frameNum);
+
+            return result;
+        });
+    }
+
+
+    //Generate a linspaced array
+    private static float[] linspace(float x1, float x2, int n)
+    {
+        float step = (x2-x1)/(n-1);
+        float[] linspaced_arr = new float[n];
+        for (int i = 0; i < n; i++)
+        {
+            linspaced_arr[i] = x1 + step*(i);
+        }
+
+        return linspaced_arr;
+    }
+}
