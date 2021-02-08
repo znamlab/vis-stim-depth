94fd16dd01edc4899cd2e2e67de63a3265c2928c
yiran25 <yiran.he@crick.ac.uk>
2021-02-08 18:28:32 +0000

    Update all files

---
+++ C#/testMath/testMath/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace testMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Cos(1));
+        }
+    }
+}
+
+
